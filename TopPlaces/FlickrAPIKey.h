695b9ab4adcb1934757987d16397468938e35925
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2012-08-02 11:19:16 -0700

    add FlickrFetcher files

---
+++ TopPlaces/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"47e64de70f617377ec72f27b90d496f2"
