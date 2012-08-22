f5bb3af12531e4297303146f79ada1c33dc3fa65
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2012-08-21 17:22:32 -0700

    Can work on iPad. Going to reduce lines of code and make the image displaying better.

--- TopPlaces/PhotoDisplayViewController.m
+++ TopPlaces/PhotoDisplayViewController.m
@@ -19,9 +19,79 @@
 @synthesize imageScrollView = _imageScrollView;
 @synthesize imageView = _imageView;
 @synthesize splitViewBarButtonItem = _splitViewBarButtonItem;
-// Can get a UIToolBar from self.navigationController.toolbar
 
 
+
+#pragma mark - UISplitViewController Button Dance
+
+- (void)handleSplitViewBarButtonItem:(UIBarButtonItem *)splitViewBarButtonItem
+{
+    NSMutableArray *toolbarItems = [self.navigationController.toolbar.items mutableCopy];
+    if (_splitViewBarButtonItem) [toolbarItems removeObject:_splitViewBarButtonItem];
+    if (splitViewBarButtonItem) [toolbarItems insertObject:splitViewBarButtonItem atIndex:0];
+    self.navigationController.toolbar.items = toolbarItems;
+    _splitViewBarButtonItem = splitViewBarButtonItem;
+}
+
+- (void)setSplitViewBarButtonItem:(UIBarButtonItem *)splitViewBarButtonItem
+{
+    if (splitViewBarButtonItem != _splitViewBarButtonItem) {
+        [self handleSplitViewBarButtonItem:splitViewBarButtonItem];
+    }
+}
+
+
+#pragma mark - UISplitViewController delegate
+
+
+
+// By using the protocol, it can work blindly with any detail view controller
+// As long as that detailViewController has a property as a button
+- (id <splitViewBarButtonPresenter>)splitViewBarButtonItemPresenter
+{
+	// It seems the last object is the navigation controller
+	id detailVC = [self.splitViewController.viewControllers lastObject];
+	if ([detailVC isKindOfClass:[UINavigationController class]]) {
+		detailVC = [detailVC topViewController];
+	}
+	if (![detailVC conformsToProtocol:@protocol(splitViewBarButtonPresenter)]) {
+		detailVC = nil;
+	}
+	return detailVC;
+}
+
+- (BOOL)splitViewController:(UISplitViewController *)svc
+   shouldHideViewController:(UIViewController *)aViewController
+			  inOrientation:(UIInterfaceOrientation)orientation
+{
+	return [self splitViewBarButtonItemPresenter] ? UIInterfaceOrientationIsPortrait(orientation) : NO;
+}
+
+
+- (void)splitViewController:(UISplitViewController *)svc
+	 willHideViewController:(UIViewController *)aViewController
+		  withBarButtonItem:(UIBarButtonItem *)barButtonItem
+	   forPopoverController:(UIPopoverController *)pc
+{
+	barButtonItem.title = self.title;
+	// Tell the detail view to put this button up
+	[self splitViewBarButtonItemPresenter].splitViewBarButtonItem = barButtonItem;
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+	 willShowViewController:(UIViewController *)aViewController
+  invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
+{
+	// Tell the detail view to take the button away
+	[self splitViewBarButtonItemPresenter].splitViewBarButtonItem = nil;
+}
+
+
+
+
+#pragma mark - Set the model
+
+// When the model is set, reset the image
 - (void)setPhotoURL:(NSURL *)photoURL
 {
 	if (_photoURL != photoURL) {
@@ -29,6 +99,9 @@
 		self.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:photoURL]];
 	}
 }
+
+
+#pragma mark - View Life Cycle
 
 
 // Set myself as the delegat of the splitViewController
@@ -65,6 +138,8 @@
 {
 	return self.imageView;
 }
+
+
 
 
 
